58db04a3e48042176fc9db22798586158c254245
jfmcelroy <105364772+jfmcelroy@users.noreply.github.com>
2022-05-13 11:03:48 -0400

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,35 +6,21 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
-months = 1 
+months = 0 
+
 extra_payment_start_month = float(input('Enter extra payment start month:'))
 extra_payment_end_month = float(input('Enter extra payment end month:'))
 extra_payment = float(input('Enter extra payment amount:'))
 
-while principal > 0 and months < extra_payment_start_month: 
+while principal > 0:
+    months = months+1
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
-    print('Month:',months,'|', 'Total paid:',round(total_paid,2),'|','Remaining principal:',round(principal,2)) 
-    months = months+1
     
-print('__________________________________________________________________________')    
-print('___________________________Extra payment starts___________________________') 
-print('__________________________________________________________________________') 
+    if months >= extra_payment_start_month and months <= extra_payment_end_month:
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
+        
+    print('Month:',months,'|', 'Total paid:',round(total_paid,2),'|','Remaining principal:',round(principal,2))
 
-while principal > 0 and months >= extra_payment_start_month and months <= extra_payment_end_month: 
-    principal = principal * (1+rate/12) - (payment+extra_payment)
-    total_paid = total_paid + (payment+extra_payment)
-    print('Month:',months,'|', 'Total paid:',round(total_paid,2),'|','Remaining principal:',round(principal,2)) 
-    months = months+1
-    
-print('________________________________________________________________________') 
-print('___________________________Extra payment ends___________________________') 
-print('________________________________________________________________________') 
-    
-while principal > 0 and months > extra_payment_end_month: 
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
-    print('Month:',months,'|', 'Total paid:',round(total_paid,2),'|','Remaining principal:',round(principal,2)) 
-    months = months+1
-    
 print('Total paid:', round(total_paid,2),'over',months-1,'months')

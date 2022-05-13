8c7b08759e19a9e75378e014556f2592e432ed19
jfmcelroy <105364772+jfmcelroy@users.noreply.github.com>
2022-05-13 10:42:59 -0400

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -7,9 +7,9 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 months = 0 
-extra_payment_start_month = input('Enter extra payment start month')
-extra_payment_end_month = input('Enter extra payment end month')
-extra_payment = input('Enter extra payment amount')
+extra_payment_start_month = input('Enter extra payment start month:')
+extra_payment_end_month = input('Enter extra payment end month:')
+extra_payment = input('Enter extra payment amount:')
 
 while principal > 0 and months < extra_payment_start_month: #watch out for edge cases
     principal = principal * (1+rate/12) - payment

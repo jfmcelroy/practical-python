282d4c20d03af6a5b93a77e6a2d7a21d8c584771
jfmcelroy <105364772+jfmcelroy@users.noreply.github.com>
2022-05-13 10:41:25 -0400

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,20 +6,31 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
-months = 0 #or should this be 1?
+months = 0 
+extra_payment_start_month = input('Enter extra payment start month')
+extra_payment_end_month = input('Enter extra payment end month')
+extra_payment = input('Enter extra payment amount')
 
-while principal > 0 and months < 12: #watch out for edge cases
-    principal = principal * (1+rate/12) - (payment+1000)
-    total_paid = total_paid + (payment+1000)
-    months = months+1
-    # print('Month:',months,'|', 'Total paid:',round(total_paid,2)) # debugging
-    
-# print('___________________________WHILE SWITCH___________________________') # debugging
-    
-while principal > 0 and months >= 12: #do I need to specify anything about months? / do while statements overlap?
+while principal > 0 and months < extra_payment_start_month: #watch out for edge cases
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
     months = months+1
-    # print('Month:',months,'|', 'Total paid:',round(total_paid,2)) # debugging
+    print('Month:',months,'|', 'Total paid:',round(total_paid,2)) # debugging
+    
+print('___________________________WHILE SWITCH___________________________') # debugging
+
+while principal > 0 and months >= extra_payment_start_month and months < extra_payment_end_month: #watch out for edge cases
+    principal = principal * (1+rate/12) - (payment+extra_payment)
+    total_paid = total_paid + (payment+extra_payment)
+    months = months+1
+    print('Month:',months,'|', 'Total paid:',round(total_paid,2)) # debugging
+    
+print('___________________________WHILE SWITCH___________________________') # debugging
+    
+while principal > 0 and months >= extra_payment_end_month: #watch out for edge cases
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    months = months+1
+    print('Month:',months,'|', 'Total paid:',round(total_paid,2)) # debugging
     
 print('Total paid:', round(total_paid,2),'over',months,'months')

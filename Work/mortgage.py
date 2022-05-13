17f72298e643266d4db4262a666d526bc79b59e1
jfmcelroy <105364772+jfmcelroy@users.noreply.github.com>
2022-05-13 10:47:43 -0400

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -19,7 +19,7 @@ while principal > 0 and months < extra_payment_start_month: #watch out for edge 
     
 print('___________________________WHILE SWITCH___________________________') # debugging
 
-while principal > 0 and months >= extra_payment_start_month and months < extra_payment_end_month: #watch out for edge cases
+while principal > 0 and months >= extra_payment_start_month and months <= extra_payment_end_month: #watch out for edge cases
     principal = principal * (1+rate/12) - (payment+extra_payment)
     total_paid = total_paid + (payment+extra_payment)
     print('Month:',months,'|', 'Total paid:',round(total_paid,2)) # debugging
@@ -27,7 +27,7 @@ while principal > 0 and months >= extra_payment_start_month and months < extra_p
     
 print('___________________________WHILE SWITCH___________________________') # debugging
     
-while principal > 0 and months >= extra_payment_end_month: #watch out for edge cases
+while principal > 0 and months > extra_payment_end_month: #watch out for edge cases
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
     print('Month:',months,'|', 'Total paid:',round(total_paid,2)) # debugging

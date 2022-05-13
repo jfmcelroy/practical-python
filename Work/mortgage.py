1b52ef8e63dd5dd7f3153e9a53c497815f6bf8b7
jfmcelroy <105364772+jfmcelroy@users.noreply.github.com>
2022-05-13 10:19:14 -0400

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -12,10 +12,12 @@ while principal > 0 and months < 12: #watch out for edge cases
     principal = principal * (1+rate/12) - (payment+1000)
     total_paid = total_paid + payment
     months = months+1
+    print('Month:',months,'|', 'Total paid:',round(total_paid,2)) # debugging
     
-while principal > 0: #do I need to specify anything about months?
+while principal > 0 and months >= 12: #do I need to specify anything about months? / do while statements overlap?
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
     months = months+1
+    print('Month:',months,'|', 'Total paid:',round(total_paid,2)) # debugging
     
 print('Total paid:', round(total_paid,2),'over',months,'months')

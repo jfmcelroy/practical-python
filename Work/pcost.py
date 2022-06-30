00b510bfdc3555594db3580aaf5c1fb1f7b72d88
jfmcelroy <105364772+jfmcelroy@users.noreply.github.com>
2022-06-30 15:29:39 -0400

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -6,6 +6,7 @@ total_cost = 0.00
 
 with open('Data/portfolio.csv', 'rt') as f: 
   for line in f:
-    total_cost = total_cost + line.split(,)[2]
+    cost = float(line.split(,)[2])
+    total_cost = total_cost + cost
     
 print("Total cost: ", total_cost)

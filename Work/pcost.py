50cea37d70612fe054cd92275e5ae978e9b65fc7
jfmcelroy <105364772+jfmcelroy@users.noreply.github.com>
2022-07-06 16:25:13 -0400

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -4,15 +4,20 @@
 
 import csv
 
-total_cost = 0.00
+def portfolio_cost(filename):
+  total_cost = 0.00
 
-with open('Data/portfolio.csv', 'rt') as f: 
+  with open(filename, 'rt') as f: #syntax for 'filename'
   rows = csv.reader(f)
   next(rows)
   for row in rows: 
     cost = float(row[2]) #already float?
     total_cost = total_cost + cost
+
+  return round(total_cost,2)
     
-round(total_cost,2)
+file = 'Data/portfolio.csv' #change for user input
+
+portfolio_cost(file)
     
 print(f'Total cost: ${total_cost:0.2f}')

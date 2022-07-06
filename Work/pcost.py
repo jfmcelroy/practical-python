38d2838eb1615fbb2a6362248d09d9ddb774f04d
jfmcelroy <105364772+jfmcelroy@users.noreply.github.com>
2022-07-06 16:17:08 -0400

    use csv module

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,16 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.27, 1.32
+
+import csv
 
 total_cost = 0.00
 
 with open('Data/portfolio.csv', 'rt') as f: 
-  next(f)
-  for line in f:
-    cost = float(line.split(',')[2][:-1])
+  rows = csv.reader(f)
+  next(rows)
+  for row in rows: 
+    cost = float(row[2]) #already float?
     total_cost = total_cost + cost
     
 round(total_cost,2)

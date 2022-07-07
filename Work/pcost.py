be54aaff2cf0dda8bc0b72f512303e69380c9576
jfmcelroy <105364772+jfmcelroy@users.noreply.github.com>
2022-07-07 11:10:55 -0400

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -5,7 +5,9 @@
 import csv
 
 def portfolio_cost(filename):
-  total_cost = 0.00
+  total_cost = 0.00 
+  error_count - 0 # keep track of bad lines
+  output = [total_cost, error_count]
 
   with open(filename, 'rt') as f: #syntax for 'filename'
     rows = csv.reader(f)
@@ -13,12 +15,14 @@ def portfolio_cost(filename):
     for row in rows:
       try:
         cost = float(row[2]) #already float?
-        total_cost = total_cost + cost
+        total_cost += cost
       except ValueError:
-        # any way to output this?
-
-  return round(total_cost,2)
+        error_count += 1  
+        
+  return output
     
 file = 'Data/portfolio.csv' #change for user input
     
-print(f'Total cost: ${portfolio_cost(file):0.2f}')
+print(f'Total cost: ${portfolio_cost(file)[0]:0.2f}')
+
+print(f'{portfolio_cost(file)[1]} row(s) with value error')

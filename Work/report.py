c401f89369d47ecb84ac15f2c9fb8315da5759b1
jfmcelroy <105364772+jfmcelroy@users.noreply.github.com>
2022-07-11 10:23:02 -0400

    Update report.py

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,27 @@
 # report.py
 #
 # Exercise 2.4
+
+def read_portfolio(filename):
+  '''Computes the total cost (shares*price) of a portfolio file'''
+  portfolio = [] 
+  error_count = 0 
+
+  with open(filename, 'rt') as f: 
+    rows = csv.reader(f)
+    # headers = next(rows)
+    for row in rows:
+      try:
+        holding = (row[0], int(row[1]), float(row[2]))
+        portfolio.append(holding)
+      except ValueError:
+        error_count += 1  
+        
+  return portfolio
+
+file = 'Data/portfolio.csv'
+
+read_portfolio(file)
+
+for r in portfolio:
+  print r
